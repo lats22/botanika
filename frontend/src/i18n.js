9bf8287828195f763a67f004dc79286fad5c8466
lats22 <golfing.sim@gmail.com>
2026-04-19 14:11:51 +0700

    feat: add i18n configuration with 6 languages

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,2 +1,30 @@
-// i18n configuration placeholder
-export default {}
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+import en from './locales/en.json'
+import th from './locales/th.json'
+import zh from './locales/zh.json'
+import fr from './locales/fr.json'
+import es from './locales/es.json'
+import ko from './locales/ko.json'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      th: { translation: th },
+      zh: { translation: zh },
+      fr: { translation: fr },
+      es: { translation: es },
+      ko: { translation: ko }
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  })
+
+export default i18n
